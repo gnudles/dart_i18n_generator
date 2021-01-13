933ccfc1779c038b054438112c4919d76407f4f7
Orr Shalom Dvory <dvoreader@gmail.com>
2021-01-13 21:18:47 +0200

    fix version checker

--- test/i18n_json_test.dart
+++ test/i18n_json_test.dart
@@ -5,5 +5,9 @@ void main() {
   test('testSdkVersionForNullSafety', () {
     expect(testSdkVersionForNullSafety("^2.12.0"), true);
     expect(testSdkVersionForNullSafety("^2.10.0"), false);
+    expect(testSdkVersionForNullSafety(">=2.12.0-0 <3.0.0"), true);
+    expect(testSdkVersionForNullSafety(">=2.12.0 <3.0.0"), true);
+    expect(testSdkVersionForNullSafety(">=2.11.0-0 <3.0.0"), false);
+    
   });
 }
